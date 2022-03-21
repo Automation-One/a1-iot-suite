87e0c767e4a2da9827d66a8339e3ff1c1d6f192f
BoergeSt <42578719+BoergeSt@users.noreply.github.com>
2022-03-21 21:20:35 +0100

    [AutomationOne.py] Added Parameters to Custom Connections

--- AutomationOne.py
+++ AutomationOne.py
@@ -875,7 +875,10 @@ class CustomConnection(Connection):
     else:
       self.callback = lambda val: val
     self.split = config.get("split",False) #Splits the value array over multiple outnodes
-  
+    self.args = config.get("args",[])
+    if not isinstance(self.args,list):
+      self.args = [self.args]
+    self.kwargs = config.get("kwargs",{})
   def execute(self):
     super().execute()
     try:
@@ -883,7 +886,7 @@ class CustomConnection(Connection):
         value = [node.getValue() for node in self.inNode]
       else:
         value = self.inNode.getValue()
-      result = self.callback(value)
+      result = self.callback(value,*self.args,**self.kwargs)
       if self.split:
         for (node,x) in zip(self.outNode,result):
           node.setValue(x)
