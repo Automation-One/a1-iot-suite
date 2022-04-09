045b6782c8e65e05b6b371b729433b0a60bf14fb
Börge Strümpfel <boerge.struempfel@gmail.com>
2022-04-09 19:44:28 +0200

    [Installer] Added Download and Install Script

---
+++ install/downloadAndInstallAll.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -ex
+
+if [ "$#" -ge 1 ]; then
+  branch="$1"
+else
+  branch="master"
+fi
+
+tmpdir=$(mktemp -d)
+
+cd $tmpdir
+
+wget "https://github.com/Automation-One/a1-iot-suite/archive/refs/heads/$branch.zip"
+unzip $branch.zip > /dev/null
+
+install_path="$tmpdir/a1-iot-suite-$branch/install/"
+
+cd $install_path
+/bin/sh installModbus.sh
+
+cd /
+rm -r $tmpdir
