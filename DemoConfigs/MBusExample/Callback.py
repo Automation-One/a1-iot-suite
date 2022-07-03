4d5f792f3d6f41d2795355b70a5307fdca1e5d3f
Börge Strümpfel <boerge.struempfel@gmail.com>
2022-07-03 13:26:29 +0200

    [M-Bus] Added Simple Example

---
+++ DemoConfigs/MBusExample/Callback.py
@@ -0,0 +1,9 @@
+import logging
+
+
+logger = logging.getLogger("AutomationOne")
+
+
+def logValue(node):
+  """Callback function for manually logging a value"""
+  logger.info("[{}] {}".format(node.name,node.getValue()))
